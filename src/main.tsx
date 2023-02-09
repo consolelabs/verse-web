038bc5a0118f5848c433bf61727276949e6c5411
tuanddd <vincenttuandao@gmail.com>
2023-02-09 10:38:28 +0700

    chore: add construction sign in front of building and add Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,14 @@ import "@fontsource/chakra-petch/400.css";
 import "@fontsource/chakra-petch/500.css";
 import "@fontsource/chakra-petch/600.css";
 import "@fontsource/chakra-petch/700.css";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+import { SENRY_DSN } from "envs";
+
+Sentry.init({
+  dsn: SENRY_DSN,
+  integrations: [new BrowserTracing()],
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
