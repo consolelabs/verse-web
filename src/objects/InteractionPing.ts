3faf4359da6f40bcbe4a243b2cbc2032e901681e
tuanddd <vincenttuandao@gmail.com>
2023-02-14 17:12:05 +0700

    chore: fix depth and remove test data

--- src/objects/InteractionPing.ts
+++ src/objects/InteractionPing.ts
@@ -1,20 +1,26 @@
+import { TILE_SIZE } from "../constants";
+
 export class InteractionPing {
   public dot: Phaser.GameObjects.Arc;
   public ping: Phaser.GameObjects.Arc;
   public text: Phaser.GameObjects.Text;
 
   constructor(public scene: Phaser.Scene, public x: number, public y: number) {
-    this.dot = scene.add.arc(x, y, 10, 0, 360, undefined, 0xf2f2f2).setDepth(y);
+    const depth = y / TILE_SIZE;
+
+    this.dot = scene.add
+      .arc(x, y, 10, 0, 360, undefined, 0xf2f2f2)
+      .setDepth(depth);
     this.ping = scene.add
       .arc(x, y, 10, 0, 360, undefined, 0xf2f2f2)
-      .setDepth(y);
+      .setDepth(depth);
 
     this.text = scene.add
       .text(x, y, "?", {
         fontSize: "14px",
         color: "#000000",
       })
-      .setDepth(y);
+      .setDepth(depth);
     this.text.setOrigin(0.5, 0.5);
 
     scene.events.on("update", this.update, this);
