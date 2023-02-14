8957592d66110d3e0a66e05ddd332d14e89c6258
tuanddd <vincenttuandao@gmail.com>
2023-02-14 12:22:07 +0700

    fix: title bg not loaded in time

--- src/objects/TitleBg.ts
+++ src/objects/TitleBg.ts
@@ -6,8 +6,8 @@ export class TitleBg {
   constructor(props: { scene: Phaser.Scene }) {
     const { scene } = props;
 
-    const cameraWidth = scene.cameras.main.width;
-    const cameraHeight = scene.cameras.main.height;
+    const cameraWidth = scene.cameras.main?.width ?? 0;
+    const cameraHeight = scene.cameras.main?.height ?? 0;
 
     this.instance = scene.add.image(
       window.innerWidth / 2,
