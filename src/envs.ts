815d05ba67c8020a4d1fee71c7372253f02592fb
TuanD <vincenttuandao@gmail.com>
2023-02-27 14:48:47 +0700

    chore: add chat channel id env (#24)

--- src/envs.ts
+++ src/envs.ts
@@ -5,5 +5,6 @@ export const API_POD_BASE_URL = import.meta.env.VITE_API_POD_BASE_URL || "";
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 export const SENRY_DSN = import.meta.env.VITE_SENTRY_DSN || "";
 export const API_WEBSOCKET_URL = import.meta.env.VITE_API_WEBSOCKET_URL || "";
+export const CHAT_CHANNEL_ID = import.meta.env.VITE_CHAT_CHANNEL_ID || "";
 export const NEKO_COL = "0x7aCeE5D0acC520faB33b3Ea25D4FEEF1FfebDE73";
 export const RABBY_COL = "0x7D1070fdbF0eF8752a9627a79b00221b53F231fA";
