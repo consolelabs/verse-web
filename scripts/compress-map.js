3b70f3f2a6c6204d7cf54f07135c2cff450bb5cf
tuanddd <vincenttuandao@gmail.com>
2023-03-10 15:36:02 +0700

    fix: read json config file

--- scripts/compress-map.js
+++ scripts/compress-map.js
@@ -1,17 +1,20 @@
 import { stringify } from "zipson";
 import { readFile, writeFile } from "fs/promises";
-import config from "../public/config.json" assert { type: "json" };
 import p from "path";
 
 console.log("Begin compressing maps");
-Object.values(config.maps).forEach((mapConfig) => {
-  readFile(p.join("./public", mapConfig.raw), { encoding: "utf8" }).then(
-    (jsonString) => {
-      const json = JSON.parse(jsonString);
-      writeFile(p.join("./public", mapConfig.compress), stringify(json));
-      console.log(
-        `Done compress map ${mapConfig.raw} -> ${mapConfig.compress}`
-      );
-    }
-  );
+readFile(new URL("../public/config.json", import.meta.url)).then((content) => {
+  const config = JSON.parse(content);
+
+  Object.values(config.maps).forEach((mapConfig) => {
+    readFile(p.join("./public", mapConfig.raw), { encoding: "utf8" }).then(
+      (jsonString) => {
+        const json = JSON.parse(jsonString);
+        writeFile(p.join("./public", mapConfig.compress), stringify(json));
+        console.log(
+          `Done compress map ${mapConfig.raw} -> ${mapConfig.compress}`
+        );
+      }
+    );
+  });
 });
